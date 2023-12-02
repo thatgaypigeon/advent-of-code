8bbc013acabef79d70be0f3775bfd0ae71ba2150
Pigeon <68453978+thatgaypigeon@users.noreply.github.com>
2023-12-02 20:23:52 +0000

    Create 02a.py

---
+++ 2023/02/02a.py
@@ -0,0 +1,20 @@
+possible = set()
+not_possible = set()
+
+with open("2023/02/input.txt", "r") as f:
+    for line in f.readlines():
+        game, rounds = line.strip().split(": ", 1)
+        game_id = int(game.split(" ")[1])
+        possible.add(game_id)
+
+        for round_data in rounds.split("; "):
+            round = dict((k, int(v)) for k, v in (reversed(s.split(" ")) for s in round_data.split(", ")))
+
+            if not (
+                (round.get("red") or 0) <= 12 and
+                (round.get("green") or 0) <= 13 and
+                (round.get("blue") or 0) <= 14
+            ):
+                not_possible.add(game_id)
+
+print(sum(possible.difference(not_possible)))
