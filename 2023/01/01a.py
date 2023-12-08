730877a5f9320cfdc7f92a36b3a1dcd9fec72284
Pigeon <pigeon43.stuff@gmail.com>
2023-12-08 19:15:34 +0000

    Day 1 (updated)

--- 2023/01/01a.py
+++ 2023/01/01a.py
@@ -1,10 +1,10 @@
-import re
+from re import findall as fa
 
 sum = 0
 
 with open("input.txt", "r") as f:
-    for index, line in enumerate(f.readlines()):
-        match = re.findall(r"\d", line)
+    for line in f.readlines():
+        match: list[str] = fa(r"\d", line)
         sum += int(match[0] + match[-1])
 
 print(sum)
--- 2023/01/01b.py
+++ 2023/01/01b.py
@@ -1,15 +1,31 @@
-import re
+from re import findall as fa
+
+WORDS: list[str] = [
+    "N/A",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
 
 sum = 0
-words = ["N/A", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 with open("input.txt", "r") as f:
     for index, line in enumerate(f.readlines()):
-        matches = re.findall(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line)
+        matches: list[str] = fa(
+            r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))", line
+        )
 
-        digit1 = words.index(matches[0]) if matches[0] in words else matches[0]
-        digit2 = words.index(matches[-1]) if matches[-1] in words else matches[-1]
+        d1: int | str = WORDS.index(matches[0]) if matches[0] in WORDS else matches[0]
+        d2: int | str = (
+            WORDS.index(matches[-1]) if matches[-1] in WORDS else matches[-1]
+        )
 
-        sum += int(str(digit1) + str(digit2))
+        sum += int(str(d1) + str(d2))
 
 print(sum)
