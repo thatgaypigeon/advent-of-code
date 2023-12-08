2fcfb135097a5fe4b57cbdb02c06ccf2f79136cd
Pigeon <pigeon43.stuff@gmail.com>
2023-12-08 19:15:42 +0000

    Day 2 (updated)

--- 2023/02/02a.py
+++ 2023/02/02a.py
@@ -3,17 +3,23 @@ not_possible = set()
 
 with open("input.txt", "r") as f:
     for line in f.readlines():
+        game: str
+        rounds: str
         game, rounds = line.strip().split(": ", 1)
+
         game_id = int(game.split(" ")[1])
         possible.add(game_id)
 
         for round_data in rounds.split("; "):
-            round = dict((k, int(v)) for k, v in (reversed(s.split(" ")) for s in round_data.split(", ")))
+            round = dict(
+                (k, int(v))
+                for k, v in (reversed(s.split(" ")) for s in round_data.split(", "))
+            )
 
             if not (
-                (round.get("red") or 0) <= 12 and
-                (round.get("green") or 0) <= 13 and
-                (round.get("blue") or 0) <= 14
+                (round.get("red") or 0) <= 12
+                and (round.get("green") or 0) <= 13
+                and (round.get("blue") or 0) <= 14
             ):
                 not_possible.add(game_id)
 
--- 2023/02/02b.py
+++ 2023/02/02b.py
@@ -2,21 +2,24 @@ sum = 0
 
 with open("input.txt", "r") as f:
     for line in f.readlines():
+        game: str
+        rounds: str
         game, rounds = line.strip().split(": ", 1)
 
-        red = 0
-        green = 0
-        blue = 0
+        red: int = 0
+        green: int = 0
+        blue: int = 0
 
         for round_data in rounds.split("; "):
-            round = dict((k, int(v)) for k, v in (reversed(s.split(" ")) for s in round_data.split(", ")))
+            round = dict(
+                (k, int(v))
+                for k, v in (reversed(s.split(" ")) for s in round_data.split(", "))
+            )
 
             red = (round.get("red") or 0) > red and round.get("red") or red
             blue = (round.get("blue") or 0) > blue and round.get("blue") or blue
             green = (round.get("green") or 0) > green and round.get("green") or green
 
-        power = red * green * blue
-
-        sum += power
+        sum += red * green * blue
 
 print(sum)
