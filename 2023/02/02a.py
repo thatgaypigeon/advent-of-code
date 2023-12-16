7464a511447f3c20225cd2e626509bc2f38af756
Pigeon <pigeon43.stuff@gmail.com>
2023-12-16 21:46:17 +0000

    Day 2 (updated)

--- 2023/02/02a.py
+++ 2023/02/02a.py
@@ -1,26 +1,22 @@
-possible = set()
-not_possible = set()
+MAX_CUBES: dict[str, int] = {"red": 12, "green": 13, "blue": 14}
+
+
+def invalid_game(game_data: str) -> bool | None:
+    for r in game_data.split("; "):
+        for colour_data in r.split(", "):
+            count, colour = colour_data.split()
+
+            if int(count) > MAX_CUBES.get(colour):
+                return True
+
 
 with open("input.txt", "r") as f:
-    for line in f.readlines():
-        game: str
-        rounds: str
-        game, rounds = line.strip().split(": ", 1)
+    game_sum = 0
 
-        game_id = int(game.split(" ")[1])
-        possible.add(game_id)
+    for game in f.readlines():
+        game_id, game_data = game.split(": ")
 
-        for round_data in rounds.split("; "):
-            round = dict(
-                (k, int(v))
-                for k, v in (reversed(s.split(" ")) for s in round_data.split(", "))
-            )
+        if not invalid_game(game_data):
+            game_sum += int(game_id.split()[1])
 
-            if not (
-                (round.get("red") or 0) <= 12
-                and (round.get("green") or 0) <= 13
-                and (round.get("blue") or 0) <= 14
-            ):
-                not_possible.add(game_id)
-
-print(sum(possible.difference(not_possible)))
+print(game_sum)
